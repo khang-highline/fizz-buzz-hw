4d591c62d82eb7ad5f0670544b0a8db4e0afdd63
khang-highline <baokhang@students.highline.edu>
2022-01-31 23:19:57 -0800

    Fix logic error: divisible by both 3 and 5 should print FizzBuzz

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -4,12 +4,12 @@ int main()
 {
 	int i;
 	for (i = 1; i <= 100; i++) {
-		if ((i % 3) == 0)
+		if ((i % 15) == 0)
+			std::cout << "FizzBuzz";
+		else if ((i % 3) == 0)
 			std::cout << "Fizz";
 		else if ((i % 5) == 0)
 			std::cout << "Buzz";
-		else if ((i % 15) == 0)
-			std::cout << "FizzBuzz";
 		else
 			std::cout << i;
 		std::cout << '\n';
